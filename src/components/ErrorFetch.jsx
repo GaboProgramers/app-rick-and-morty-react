40bcc725b3cdec4956f1ba9938caa29898c77b60
GaboProgramers <gutierrezyosguti@gmail.com>
2022-12-05 19:28:40 -0500

    arreglo de error

--- src/components/ErrorFetch.jsx
+++ src/components/ErrorFetch.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const ErrorFetch = () => {
     return (
-        <h1>✖️ "You just have to enter numbers from 1 to 126"</h1>
+        <div className='error__container'>
+            <div className='error__icon'>
+                <i class='bx bx-error-alt'></i>
+            </div>
+            <h1 className='error__title'>"You just have to enter numbers from 1 to 126"</h1>
+        </div>
     )
 }
 
